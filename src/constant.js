aa82b740d27793c0dda01a800b078e332aec28d9
arnoldatk94 <arnoldatk94@gmail.com>
2023-05-17 10:47:33 +0800

    install netlify

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,6 @@
-export const BACKEND_URL = "http://localhost:8000";
+// export const BACKEND_URL = "https://facilbookbackend.fly.dev";
+
+export const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://facilbookbackend.fly.dev"
+    : "http://localhost:3000";
